fbf1e6e0ba66453868231086d0699828568c6028
GeniusCorn <hi@nicecorn.com>
2022-11-07 23:30:57 +0800

    chore: update user script info

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         juejin-links-auto-jump
-// @namespace    https://github.com/GeniusCorn/juejin-links-auto-jump
-// @version      0.1
-// @description  Auto skipping links in Juejin.
-// @author       GeniusCorn
-// @match        *://link.juejin.cn/*
+// @name             自动跳转掘金链接
+// @name:en          juejin-links-auto-jump
+// @namespace        https://github.com/GeniusCorn/juejin-links-auto-jump
+// @version          0.1
+// @description      自动跳转掘金中的链接。
+// @description:en   Auto skipping links in Juejin.
+// @author           GeniusCorn
+// @match            *://link.juejin.cn/*
 // ==/UserScript==
 
 ;(function () {
