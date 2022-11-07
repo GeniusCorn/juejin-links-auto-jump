511e5b0fd09cd8113152bedec6d13286f6284df5
GeniusCorn <hi@nicecorn.com>
2022-11-07 23:23:28 +0800

    feat: auto skipping in Juejin

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         juejin-links-auto-jump
+// @namespace    https://github.com/GeniusCorn/juejin-links-auto-jump
+// @version      0.1
+// @description  Auto skipping links in Juejin.
+// @author       GeniusCorn
+// @match        *://link.juejin.cn/*
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+  const button = document.querySelector('.btn')
+
+  button.click()
+})()
